97331dde391ae4a2bcfd72fe8c34e2f27339b0de
Benjamen Keroack <ben@keroack.com>
2019-02-17 17:28:23 -0800

    initial commit

---
+++ git.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# bash shortcuts for git
+
+gs() {
+    git status $@
+}
+
+gk() {
+    git checkout $@
+}
+
+ga() {
+    git add $@
+}
+
+gaa() {
+    git add --all $@
+}
+
+gb() {
+    git branch $@
+}
+
+gp() {
+    git push $@
+}
+
+gc() {
+    git commit -m "$1"
+}
+
+gm() {
+    git merge $@
+}
+
+gd() {
+    git diff $@
+}
+
+gr() {
+    git reset $@
+}
